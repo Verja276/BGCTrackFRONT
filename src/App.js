041f9499588630960cf99ea1bad2e52ae5fe81c0
vma21 <vma21@sfu.ca>
2022-03-23 15:22:20 -0700

    fix routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import LoginHome from "./LoginHome";
 import Signup from "./Signup";
+import BarcodeScan from "./BarcodeScan.js";
+import SearchEquip from "./SearchEquip";
 
 
 function App() {
@@ -12,11 +14,13 @@ function App() {
           <Routes>
             <Route path="/" element={<LoginHome />} />
             <Route path="/Signup" element={<Signup />} />
+            <Route path="/BarcodeScan" element={<BarcodeScan />} />
+            <Route path="/SearchEquip" element={<SearchEquip />} />
           </Routes>
         </Router>
       </>
   );
-  
+
 }
 
 export default App;
