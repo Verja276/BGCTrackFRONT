4a3798eba409b6cc9ce36fd90e4bb6a417702081
vma21 <vma21@sfu.ca>
2022-03-24 19:50:14 -0700

    it2

---
+++ src/SignUpEquip.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import background from "./background5.jpg";
+import BarcodeScannerComponent from "react-webcam-barcode-scanner";
+import { Navigate } from 'react-router-dom';
+import Pro from "./notBasic/SignUpEquipPro";
+//import Signup from "./Signup";
+
+function SignUpEquip() {
+    return (
+        (sessionStorage.getItem("user_status") == "a") ? (
+            <Pro />
+        ) : ((sessionStorage.getItem("user_status") == "e") ? (<Pro />) : 
+        (<Navigate to="/" replace={true} />))
+
+    );
+}
+
+
+
+
+
+export default SignUpEquip;
