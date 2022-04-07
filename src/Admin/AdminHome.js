12c83df94368d5f508cbc3701622d90a73dedcfd
vma21 <vma21@sfu.ca>
2022-04-07 14:37:18 -0700

    added session tracker

--- src/Admin/AdminHome.js
+++ src/Admin/AdminHome.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../App.css";//
-import { useState } from "react";
 import { Link } from 'react-router-dom';
 import { Helmet } from "react-helmet";
 import BarcodeScannerComponent from "react-webcam-barcode-scanner";
