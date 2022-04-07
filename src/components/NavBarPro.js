8a101351a3718a522eb6151a8f61c22d7b7cd556
vma21 <vma21@sfu.ca>
2022-04-07 16:08:11 -0700

    added session tracker

--- src/components/NavBarPro.js
+++ src/components/NavBarPro.js
@@ -65,21 +65,32 @@ export default function () {
 
         ) : (
             <Container>
-                <Navbar.Brand onClick={handleCleanStorage} href="/">{<div> <img src={photo} height="45" width="45" alt="LOGO" /> </div>}
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
+                <Navbar bg="light" expand="lg">
+                    <Navbar.Brand onClick={handleCleanStorage} href="/">{<div> <img src={photo} height="45" width="45" alt="LOGO" /> </div>}
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
 
-                        <NavDropdown title="Other" id="basic-nav-dropdown">
-                        <NavDropdown.Item onClick={handleCleanStorage} href="/yourequip">Your Equipment</NavDropdown.Item>
-                            <NavDropdown.Item onClick={handleCleanStorage} href="/SearchEquip">Search Equipment</NavDropdown.Item>
-                            <NavDropdown.Item onClick={handleCleanStorage} href="/RequestEquip">Request Equipment</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container >
+
+                            <NavDropdown title="Other" id="basic-nav-dropdown">
+                            <NavDropdown.Item onClick={handleCleanStorage} href="/yourequip">Your Equipment</NavDropdown.Item>
+                                <NavDropdown.Item onClick={handleCleanStorage} href="/SearchEquip">Search Equipment</NavDropdown.Item>
+                                <NavDropdown.Item onClick={handleCleanStorage} href="/SearchUser">Search Users</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item onClick={handleCleanStorage} href="/BarcodeScan">Add Equipment</NavDropdown.Item>
+                                <NavDropdown.Item onClick={handleCleanStorage} href="/Signup">Add User</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item onClick={handleCleanStorage} href="/RequestEquip">Request Equipment</NavDropdown.Item>
+                                <NavDropdown.Item onClick={handleCleanStorage} href="/manageRequests">Check Out</NavDropdown.Item>
+                                <NavDropdown.Item onClick={handleCleanStorage} href="/CheckIn">Check In</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+
+                </Navbar>
+            </Container>
         ))
     );
 
